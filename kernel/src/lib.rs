7489a7b380abec3520b3fc3b753114fcdfdd41f7
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2023-07-21 06:57:42 +0200

    how to implement hello world

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,5 +1,10 @@
-use tezos_smart_rollup::{kernel_entry, prelude::Runtime};
+use tezos_smart_rollup::{
+    kernel_entry,
+    prelude::{debug_msg, Runtime},
+};
 
-pub fn entry(rt: &mut impl Runtime) {}
+pub fn entry(rt: &mut impl Runtime) {
+    debug_msg!(rt, "Hello TezDev 2023!!!\n");
+}
 
 kernel_entry!(entry);
