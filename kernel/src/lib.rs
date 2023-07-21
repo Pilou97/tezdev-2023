113c4c8c6361cde7f0e599ca932acb0b9ce745f5
Pierre-Louis Dubois <pierrelouis.dubois@tutanota.com>
2023-07-21 07:10:51 +0200

    how to read messages from the L1

--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,6 +1,9 @@
 use tezos_data_encoding::{enc::BinWriter, nom::NomReader};
 use tezos_smart_rollup::{
+    dac::certificate::Certificate,
+    inbox::InboxMessage,
     kernel_entry,
+    michelson::MichelsonUnit,
     prelude::{debug_msg, Runtime},
     storage::path::RefPath,
 };
@@ -25,11 +28,39 @@ fn save_player(rt: &mut impl Runtime, player: Player) {
     let _ = rt.store_write_all(&PLAYER_PATH, &bytes);
 }
 
+#[derive(Debug, NomReader)]
+enum Message {
+    UpgradeRequest(Certificate),
+}
+
+fn read_message(rt: &mut impl Runtime) -> Option<Message> {
+    let input = rt.read_input().ok()??;
+    let _ = rt.mark_for_reboot();
+
+    let (_, message) = InboxMessage::<MichelsonUnit>::parse(input.as_ref()).ok()?;
+    debug_msg!(rt, "{message:?}\n");
+
+    let InboxMessage::External(payload) = message else { return None };
+
+    let (_, message) = Message::nom_read(payload).ok()?;
+
+    Some(message)
+}
+
 pub fn entry(rt: &mut impl Runtime) {
     debug_msg!(rt, "Hello TezDev 2023!!!\n");
 
     let player = load_player(rt);
     let player = player.unwrap_or_default();
+
+    if let Some(message) = read_message(rt) {
+        debug_msg!(rt, "message: {message:?}\n");
+        match message {
+            Message::UpgradeRequest(_cert) => {
+                // todo
+            }
+        };
+    };
 
     save_player(rt, player);
 }
